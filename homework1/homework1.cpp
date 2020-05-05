4fe4eef72353ca923dc9e9bdd50e59a0b8395e17
Naga Nithin Manne <nithinmanne@gmail.com>
2020-05-04 22:24:32 -0500

    Finished homework4

--- homework1/homework1.cpp
+++ homework1/homework1.cpp
@@ -2,8 +2,51 @@
 //
 
 #include <iostream>
+#include <ostream>
+#include <iterator>
+#include <vector>
+
+#include <boost/iterator/function_output_iterator.hpp>
+
+template<typename T, typename CharT = char, typename Traits = std::char_traits<CharT>>
+class _ostream_joiner_helper {
+private:
+    /* Storing Pointer to stream, since boost::function_output_iterator
+       requires what we pass to it to be copy constructible/assignable,
+       which is not possible since storing a reference would not keep it
+       assignable, and storing a copy is not possible, since basic_ostream
+       is not copyable. Note that, using references seems to actually work
+       in G++ 9 in Linux, but MSVC complains about the implicitly deleted
+       copy assignment. */
+    std::basic_ostream<CharT, Traits>* stream_ptr;
+    CharT const* delim;
+    bool first = true;
+public:
+    _ostream_joiner_helper(std::basic_ostream<CharT, Traits>& stream, CharT const* delim) :
+        stream_ptr{ &stream }, delim{ delim } {}
+
+    void operator()(T const& t) {
+        if (first) first = false;
+        else  (*stream_ptr) << delim;
+        (*stream_ptr) << t;
+    }
+};
+
+template<typename T, typename CharT = char, typename Traits = std::char_traits<CharT>>
+auto ostream_joiner(std::basic_ostream<CharT, Traits>& stream, CharT const* delim = "\0") {
+    return boost::function_output_iterator(_ostream_joiner_helper<T>(stream, delim));
+}
+
+template<typename T, typename CharT = char, typename Traits = std::char_traits<CharT>>
+std::basic_ostream<CharT, Traits>& operator<<(std::basic_ostream<CharT, Traits>& stream, std::vector<T> vector) {
+    stream << "[ ";
+    std::copy(vector.begin(), vector.end(), ostream_joiner<T>(stream, ", "));
+    stream << " ]";
+    return stream;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::vector<double> double_vec{ 1., 2.71, 3.1415, 4., 5., 6.66 };
+    std::cout << double_vec << std::endl;
 }
--- homework2/homework2.cpp
+++ homework2/homework2.cpp
@@ -2,8 +2,58 @@
 //
 
 #include <iostream>
+#include <ostream>
+#include <sstream>
+#include <string>
+
+template<typename CharT = char, typename Traits = std::char_traits<CharT>>
+class IndentStreamBuf : public std::basic_stringbuf<CharT, Traits> {
+private:
+    std::basic_ostream<CharT, Traits>& ostream;
+    int indent_size;
+    typename std::basic_string<CharT>::size_type indent_count = 0;
+public:
+    IndentStreamBuf(std::basic_ostream<CharT, Traits>& ostream, int indent_size = 4) : ostream(ostream), indent_size(indent_size) {}
+    int overflow(int ch) override {
+        CharT char_ch = static_cast<CharT>(ch);
+        ostream << char_ch;
+        if (char_ch == '\n')
+            ostream << std::basic_string<CharT>(indent_count*indent_size, ' ');
+        return ch;
+    }
+    void indent() { indent_count++; }
+    void unindent() { indent_count--; }
+};
+
+template<typename CharT = char, typename Traits = std::char_traits<CharT>>
+class IndentStream : public std::basic_ostream<CharT, Traits> {
+    IndentStreamBuf<CharT, Traits> streambuf;
+public:
+    IndentStream(std::basic_ostream<CharT, Traits>& ostream, int indent_size = 4) : streambuf(ostream, indent_size), std::basic_ostream<CharT, Traits>(&streambuf) {}
+};
+
+template<typename CharT = char, typename Traits = std::char_traits<CharT>>
+std::basic_ostream<CharT, Traits>& indent(std::basic_ostream<CharT, Traits>& os) {
+    static_cast<IndentStreamBuf<CharT, Traits>*>(os.rdbuf())->indent();
+    return os;
+}
+template<typename CharT = char, typename Traits = std::char_traits<CharT>>
+std::basic_ostream<CharT, Traits>& unindent(std::basic_ostream<CharT, Traits>& os) {
+    static_cast<IndentStreamBuf<CharT, Traits>*>(os.rdbuf())->unindent();
+    return os;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    using std::cout;
+    using std::endl;
+    IndentStream ins(cout);
+    ins << "int" << endl;
+    ins << "fib(int n) {" << indent << endl;
+    ins << "if (n == 0) " << indent << endl;
+    ins << "return 0;" << unindent << endl;
+    ins << "if (n == 1) " << indent << endl;
+    ins << "return 1;" << unindent << endl;
+    ins << "return fib(n-2) + fib(n-1);" << unindent << endl;
+    ins << "}" << endl;
 }
