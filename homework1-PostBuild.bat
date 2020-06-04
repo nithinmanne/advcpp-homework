17aaeab5fcb108f471770b7fb0f8fba92622d241
Naga Nithin Manne <nithinmanne@gmail.com>
2020-06-04 14:08:26 -0500

    Auto generate header and support Release

---
+++ homework1-PostBuild.bat
@@ -0,0 +1,4 @@
+echo off
+"%~1\%~2\homework1-xmlDataBind" "%~1\homework1-Note\XMLSchema1.xsd" "%~1\homework1-Note\note.h"
+"%~1\%~2\homework1-xmlDataBind" "%~1\homework2\XMLSchema1.xsd" "%~1\homework2\note.h"
+"%~1\%~2\homework1-xmlDataBind" "%~1\homework3\XMLSchema1.xsd" "%~1\homework3\note.h"
